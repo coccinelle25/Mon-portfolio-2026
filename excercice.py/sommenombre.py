ec2b65e4aba48c696d0920380914b0ff3bab0907
=vincent MBUZA MULUMBI <=vincentmbuza2@gmail.com>
2026-03-19 15:10:46 +0100

    Stylisation du header

---
+++ excercice.py/sommenombre.py
@@ -0,0 +1,17 @@
+"""nb = 1
+nombre =int(input("saisir un nombre: "))
+somme = nb + nombre
+for i in range(1,nombre):
+            somme += i
+            print(somme)"""
+import time
+def somme1 (nb):
+    debut = time.time ()
+nb =int(input("saisir un nombre: "))
+somme = 0
+for i in range (nb + 1):
+    somme += i
+    print("somme1 =" , seconde)
+    fin = time.time
+    duree =round(fin_debut,1)
+    print(f"traitement effectué en {duree} seconde")
---
+++ excercice.py/untiled-3.py
@@ -0,0 +1,13 @@
+nombres = [3,1,5,9,2,6]
+resultat = []
+for n in nombres:
+    if n > 4:
+        resultat.append
+print(resultat)
+
+def addition(a,b)
+    return a + b
+
+def est_pair(n)
+    n = 0
+     
